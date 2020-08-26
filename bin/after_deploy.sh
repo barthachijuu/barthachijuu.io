4b4652ccc5f402916886bd6407d75f159b4d99d8
Bartolo Amico <bartolo.amico@gmail.com>
2020-08-26 11:17:38 +0200

    Deploy
    Temporary disable script on appveyor.
    Remove before_deploy script

--- bin/after_deploy.sh
+++ bin/after_deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ssh barthag@ssh.cluster028.hosting.ovh.net <<EOF
-  cd www && rm -rf *
-  unzip *.zip
+  echo "deploy the package"
+  cd www && rm *.html *.js *.gz *.txt manifest.json .htaccess && rm -r images
+  unzip *.zip && rm *.zip
 EOF
--- bin/before_deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ssh barthag@ssh.cluster028.hosting.ovh.net <<EOF
-  cd www && rm -rf *
-EOF
